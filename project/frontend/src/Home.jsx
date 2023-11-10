97797c490fa550442fb43cb412d4c248d0a03ddf
Shiya Shalmali Shetty <137711284+shiyashalmali@users.noreply.github.com>
2023-11-10 20:53:09 +0530

    Update Home.jsx

--- project/frontend/src/Home.jsx
+++ project/frontend/src/Home.jsx
@@ -1,1 +1,31 @@
+import React from "react";
+import video from "./assets/journal.mp4";
 
+
+
+import "./Home.css";
+const Home = () => { 
+    return (
+        <section className='home'>
+            <div className="overlay"></div>
+            <video src={video} muted autoPlay loop type="journal.mp4"></video>
+            <div className="textDiv">
+            <h3 className="hometitle">
+                ScribbleSymphony
+                </h3>
+                <div className="smallText">
+                Compose your thoughts, one note at a time, with ScribbleSymphony – where your ideas create the perfect harmony on the pages of your digital journal.
+                </div>
+                
+
+            </div>
+        
+           
+
+
+        </section>
+        
+       
+    )
+}
+export default Home;
