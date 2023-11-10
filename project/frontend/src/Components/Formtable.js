cdca3712bd816e86ab6627cfd3bb8073f116985c
Shiya Shalmali Shetty <137711284+shiyashalmali@users.noreply.github.com>
2023-11-10 20:56:50 +0530

    Update Journal.js

--- project/frontend/src/Components/Formtable.js
+++ project/frontend/src/Components/Formtable.js
@@ -1,1 +1,160 @@
+import './Journal.css';
+import { useEffect, useState } from 'react';
+import axios from "axios"
+import Formtable from './Formtable';
 
+axios.defaults.baseURL = "http://localhost:8080"
+
+function Journal() {
+  const [addSection,setAddSection] = useState(false)
+  const [editSection,setEditSection] = useState(false)
+  const [formData,setFormData] = useState({
+    name : "",
+    body : ""
+  })
+  const [formDataEdit,setFormDataEdit] = useState({
+    name : "",
+    body : "",
+    _id : ""
+  })
+  const [dataList,setDataList] = useState([])
+
+  const handleOnChange = (e)=>{
+    const {value,name} = e.target
+    setFormData((preve)=>{
+        return{
+          ...preve,
+          [name] : value
+        }
+    })
+  }
+
+
+  const handleSubmit = async(e)=>{
+      e.preventDefault()
+      const data = await axios.post("/create",formData)
+      console.log(data)
+      if(data.data.success){
+          setAddSection(false)
+          alert(data.data.message)
+          getFetchData()
+          setFormData({
+            name : "",
+            body : ""
+          })
+
+      }
+  }
+  const getFetchData = async()=>{
+    const data = await axios.get("/")
+    console.log(data)
+    if(data.data.success){
+        setDataList(data.data.data)
+    }
+  }
+  useEffect(()=>{
+    getFetchData()
+  },[])
+
+  const handleDelete = async(id)=>{
+    const data = await axios.delete("/delete/"+id)
+    
+      if(data.data.success){
+        getFetchData()
+        alert(data.data.message)
+      }
+  }
+
+  const handleUpdate = async(e)=>{
+    e.preventDefault()
+    const data = await axios.put("/update",formDataEdit)
+    if(data.data.success){
+      getFetchData()
+      alert(data.data.message)
+      setEditSection(false)
+    }
+  }
+  const handleEditOnChange = async(e)=>{
+    const {value,name} = e.target
+    setFormDataEdit((preve)=>{
+        return{
+          ...preve,
+          [name] : value
+        }
+    })
+  }
+  const handleEdit = (el)=>{
+    setFormDataEdit(el)
+    setEditSection(true)
+  }
+  return (
+   <>
+      <div className="container">
+        <button className="btn btn-add" onClick={()=>setAddSection(true)}>Add</button>
+
+      {
+        addSection && (
+          <Formtable
+            handleSubmit={handleSubmit}
+            handleOnChange={handleOnChange}
+            handleclose = {()=>setAddSection(false)}
+            rest={formData}
+          />
+        )
+      }
+      {
+        editSection && (
+          <Formtable
+            handleSubmit={handleUpdate}
+            handleOnChange={handleEditOnChange}
+            handleclose = {()=>setEditSection(false)}
+            rest={formDataEdit}
+          />
+        )
+      }
+
+
+      <div className='tableContainer'>
+        <table>
+          <thead>
+            <tr >
+              <th>Name</th>
+              <th>Email</th>
+              <th>
+              
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            { dataList[0] ? (
+              dataList.map((el)=>{
+                console.log(el)
+                return(
+                  <tr>
+                    <td>{el.name}</td>
+                    <td>{el.body}</td>
+                    <td>
+                      <button className='btn btn-edit' onClick={()=>handleEdit(el)}>Edit</button>
+                      <button className='btn btn-delete' onClick={()=>handleDelete(el._id)}>Delete</button>
+                    </td>
+                  </tr>
+                )
+              }))
+              : (
+                <p style={{textAlign : "center"}}>No data</p>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+     
+
+
+      </div>
+   </>
+  );
+}
+
+
+
+export default Journal;
